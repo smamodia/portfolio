12490e20991608fb6fea43bdb25d725e07ff5fdd
smamodia <161131071+smamodia@users.noreply.github.com>
2024-02-24 23:15:55 +0530

    Add files via upload

---
+++ script.js
@@ -0,0 +1,5 @@
+document.getElementById('contact-form').addEventListener('submit', function(event) {
+    event.preventDefault();
+    // Perform form submission or validation here
+    alert('Form submitted!');
+});
